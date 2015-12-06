90cf6569fefc1658d81ac8f50aa4b99715febf87
Andrew Bezzubtsev <andrew.bezzubtsev@icloud.com>
2015-12-06 21:45:37 +0300

    bugfix

--- assemble/test_assemble.c
+++ assemble/test_assemble.c
@@ -35,6 +35,9 @@ int main(int argn, char * const *argv)
 			if (!fgets(buf, sizeof(buf) - 1, input))
              		   	break;
 
+			if (*buf == '\n')
+				continue;
+
 			op = parse_code(buf);
 			dump_operation(op);
 
