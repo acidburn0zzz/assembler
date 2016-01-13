c4e9e373a6bb877a8c35e8eb9bdc3c160f8907d5
Andrew-Bezzubtsev <andrew.bezzubtsev@icloud.com>
2016-01-13 20:12:11 +0300

    Revert "invoke format"
    This reverts commit 3b45b099cb73f8d46602c2dbdef3bb7b262a4431.

---
+++ assemble/assemble.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "../parser/parser.h"
+#include "compiler.h"
+
+int main(int argn, char * const *argv)
+{
+	int a = 0;
+
+	if (argn <= 1) {
+		fprintf(stderr, "No files for compiling\n");
+		return EINVAL;
+	}
+
+	for (a = 1; a < argn; a++) {
+		FILE *input = fopen(argv[a], "r");
+
+		char out[strlen(argv[a]) + 8];
+		sprintf(out, "%s.object", argv[a]);
+		FILE *output = fopen(out, "w");
+
+		if (!input) {
+			fprintf(stderr, "Failed to open file '%s': %s\n", argv[a], strerror(errno));
+			return errno;
+		}
+
+		while (!feof(input)) {
+			char buf[1024] = "";
+			struct operation *op = 0;
+			struct cmd *command = 0;
+
+			if (!fgets(buf, sizeof(buf) - 1, input))
+             		   	break;
+
+			if (*buf == '\n' || *buf == ';')
+				continue;
+
+			op = parse_code(buf);
+			dump_operation(op);
+
+			command = assemble_operation(op);
+			dump_to_file(output, command);
+
+			free((void *)op);
+			free((void *)command);
+		}
+
+		fclose(input);
+		fclose(output);
+	}
+
+	return 0;
+}
--- makefile
+++ makefile
@@ -2,7 +2,7 @@ FLAGS:=-g -O0
 
 PARSER:=parser/parser.h parser/parser.c
 ALANYSIS:=analysis/conditions.h analysis/conditions.c analysis/commands.h analysis/commands.c analysis/analysis.h analysis/analysis.c
-ASSEMBLE:=assembler/compiler.h assembler/compiler.c assembler.c
+ASSEMBLE:=assembler/compiler.h assembler/compiler.c assembler/assemble.c
 
 all: assembler
 
